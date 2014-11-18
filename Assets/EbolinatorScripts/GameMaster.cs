684315c7e6e995e3cd8f2014a86698fc09f38dd4
unknown <djn5286@OH1380-13.main.ad.rit.edu>
2014-11-18 08:41:40 -0500

    GameMaster changes

--- Assets/EbolinatorScripts/GameMaster.cs
+++ Assets/EbolinatorScripts/GameMaster.cs
@@ -10,21 +10,53 @@ public class GameMaster : MonoBehaviour {
 	int interactionMax; //How many interactions are we allowed this level
 	int infectedQuota;	//How many people do we need to put at risk this level
 
+	public enum GameState{
+		game,
+		win,
+		lose
+	}
+
+	GameState state = GameState.game;
 	// Use this for initialization
 	void OnGUI(){
-		GUI.skin = ebolaskin;
-		GUI.Label( new Rect(50,50,177,75), "Infected: " + infectedCount + "/" + infectedQuota);
-		GUI.Label( new Rect(50,150,177,75), "Interactions: " + interactionCount + "/" + interactionMax);
+		switch(state){
+		case GameState.game:
+			GUI.skin = ebolaskin;
+			GUI.Label( new Rect(50,50,177,75), "Infected: " + infectedCount + "/" + infectedQuota);
+			GUI.Label( new Rect(50,150,177,75), "Interactions: " + interactionCount + "/" + interactionMax);
+			Update();
+			break;
+		}
 	}
 	/*Set up level
 		max - The maximum amount of interactions this level
 		quota - The amount of people to put at risk this level
 	*/
-	void SetUpLevel(int max, int quota){
+	public void SetUpLevel(int max, int quota){
 		interactionCount = 0;
 		infectedCount = 0;
 		interactionMax = max;
 		infectedQuota = quota;
 	}
+	/*Interact
+	 	points - how many people does this infect
+	*/
+	public void Interact(int points){
+		interactionCount += 1;
+		infectedCount +=points;
+	}
+	/*Update
+		check if level completed or lost
+	*/
+	void Update(){
+		if(infectedCount >= infectedQuota){
+			infectedCount = infectedQuota;
+			state = GameState.win;
+		}
 
+		if(interactionCount >= interactionMax){
+			interactionCount = interactionMax;
+			state = GameState.lose;
+		}
+	}
 }

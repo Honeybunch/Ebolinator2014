4cac0d02491b7ab4bea46c4839e67f280acd8c43
Dylan Nelkin <djn5286@rit.edu>
2014-12-13 17:46:43 -0500

    Vomit & Spit added, all done!

--- Assets/EbolinatorScripts/InteractionItemScripts/CupInteraction.cs
+++ Assets/EbolinatorScripts/InteractionItemScripts/CupInteraction.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CupInteraction : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class CupInteraction : InteractionItem
+{
+	int allowedInteractionCount = 1;
+	int usedInteractionCount = 0;
+	public ParticleSystem spit;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public override void Interaction()
+	{
+		if(usedInteractionCount >= allowedInteractionCount)
+			exhausted = true;
+		
+		if(!exhausted)
+		{
+			gameMaster.Interact(2);
+			spit.Play();
+			usedInteractionCount++;
+			StartCoroutine(ShowMessage("Spit take! +2", 2));
+		}
+		else
+		{
+			gameMaster.Interact(0);
+			ExhaustedMessage();
+		}
 	}
 }
---
+++ Assets/EbolinatorScripts/InteractionItemScripts/FoodInteraction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodInteraction : InteractionItem
+{
+	int allowedInteractionCount = 6;
+	int usedInteractionCount = 0;
+	public ParticleSystem vomit;
+	
+	public override void Interaction()
+	{
+		if(usedInteractionCount >= allowedInteractionCount)
+			exhausted = true;
+		
+		if(!exhausted)
+		{
+			if(usedInteractionCount < 5){
+				gameMaster.Interact(0);
+				StartCoroutine(ShowMessage("Whats the deal with this? +0", 2));
+			}
+			else{
+				gameMaster.Interact(10);
+				vomit.Play();
+				StartCoroutine(ShowMessage("THAR SHE BLOWS! +10", 2));
+			}
+			usedInteractionCount++;
+		}
+		else
+		{
+			gameMaster.Interact(0);
+			ExhaustedMessage();
+		}
+	}
+}
